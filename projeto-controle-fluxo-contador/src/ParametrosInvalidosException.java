2f18e36ee36c0491bbaa6ab870ee88ab2f2d2051
fgdamasceno <fgdamasceno@gmail.com>
2024-08-28 09:39:39 -0300

    projeto tratamento de excecoes

---
+++ projeto-controle-fluxo-contador/src/ParametrosInvalidosException.java
@@ -0,0 +1,8 @@
+
+public class ParametrosInvalidosException extends Exception {
+	
+	public ParametrosInvalidosException() {
+		System.out.println("O segundo parametro deve ser maior que o primeiro!");
+	}
+	
+}
