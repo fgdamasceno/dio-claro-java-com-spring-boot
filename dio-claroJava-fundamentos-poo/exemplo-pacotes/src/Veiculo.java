9feb935acfd69c37c93e5d32d53e87993922a079
fgdamasceno <fgdamasceno@gmail.com>
2024-09-02 10:15:50 -0300

    pilares poo

---
+++ dio-claroJava-fundamentos-poo/exemplo-pacotes/src/Veiculo.java
@@ -0,0 +1,12 @@
+
+public abstract class Veiculo {
+	public String chassi;
+	public String getChassi() {
+		return chassi;
+	}
+	public void setChassi(String chassi) {
+		this.chassi = chassi;
+	}
+	public abstract void ligar();
+
+}
