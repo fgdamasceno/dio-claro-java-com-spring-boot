119d44c9b7ed61d67aadf34e7384dadfde17bc6f
fgdamasceno <fgdamasceno@gmail.com>
2024-09-17 17:19:34 -0300

    java opp aulas map

--- java-collections/collections-java-api-dio-claro/src/map/pesquisa/Produto.java
+++ java-collections/collections-java-api-dio-claro/src/map/pesquisa/Produto.java
@@ -1,5 +1,33 @@
 package map.pesquisa;
 
 public class Produto {
+	// atributo
+	private String nome;
+	private double preco;
+	private int quantidade;
+
+	public Produto(String nome, double preco, int quantidade) {
+		super();
+		this.nome = nome;
+		this.preco = preco;
+		this.quantidade = quantidade;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public double getPreco() {
+		return preco;
+	}
+
+	public int getQuantidade() {
+		return quantidade;
+	}
+
+	@Override
+	public String toString() {
+		return "Produto [nome=" + nome + ", preco=" + preco + ", quantidade=" + quantidade + "]";
+	}
 
 }
